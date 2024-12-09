2dc3c806b393b3f83da0f3fe48fbbbd393651fb2
JP Halis <jphalisnj@gmail.com>
2024-12-09 17:44:49 -0500

    Add interview questions to the company pages.

--- routes/companies.js
+++ routes/companies.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 let router = Router()
-import { companiesData } from '../data/index.js'
+import { companiesData, questionData } from '../data/index.js'
 import validations from '../validations.js'
 
 router
@@ -8,17 +8,19 @@ router
   .get(async (req, res) => {
     //code here for GET
     try {
-      const companiesList = await companiesData.getCompaniesByPage(1, 60);
-      res.render('companies/companyList', { companies: companiesList });
+      const companiesList = await companiesData.getCompaniesByPage(1, 60)
+      res.render('companies/companyList', { companies: companiesList })
     } catch (e) {
-      return res.status(500).send(e);
+      return res.status(500).send(e)
     }
   })
   .post(async (req, res) => {
-    let companyInfo = req.body;
+    let companyInfo = req.body
 
-    if(!companyInfo || Object.keys(companyInfo).length === 0) {
-      return res.status(400).json({error: 'You must provide data to create a company'});
+    if (!companyInfo || Object.keys(companyInfo).length === 0) {
+      return res
+        .status(400)
+        .json({ error: 'You must provide data to create a company' })
     }
     try {
       const newCompany = await companiesData.createCompanies(
@@ -30,34 +32,32 @@ router
         companyInfo.description,
         companyInfo.size,
         companyInfo.location
-      );
-      res.status(201).json(newCompany);
+      )
+      res.status(201).json(newCompany)
     } catch (e) {
-      res.status(500).json({error: e});
+      res.status(500).json({ error: e })
     }
-  });
+  })
 
-router
-  .route('/page/:page')
-  .get(async (req, res) => {
-    let page;
+router.route('/page/:page').get(async (req, res) => {
+  let page
 
-    try {
-      page = parseInt(req.params.page, 10);
-      if (isNaN(page) || page <= 0) {
-        throw new Error('Page must be a positive integer.');
-      }
-    } catch (e) {
-      return res.status(400).json({ error: e.message });
+  try {
+    page = parseInt(req.params.page, 10)
+    if (isNaN(page) || page <= 0) {
+      throw new Error('Page must be a positive integer.')
     }
+  } catch (e) {
+    return res.status(400).json({ error: e.message })
+  }
 
-    try {
-      const companiesList = await companiesData.getCompaniesByPage(page, 60);
-      res.status(200).json(companiesList);
-    } catch (e) {
-      return res.status(404).json({ error: e.message });
-    }
-  });
+  try {
+    const companiesList = await companiesData.getCompaniesByPage(page, 60)
+    res.status(200).json(companiesList)
+  } catch (e) {
+    return res.status(404).json({ error: e.message })
+  }
+})
 
 router
   .route('/:companyId')
@@ -69,26 +69,41 @@ router
     // }
     try {
       const company = await companiesData.getCompanyById(req.params.companyId)
-      res.render('companies/company', {company: company})
+      const companyQuestions = await questionData.getCompanyQuestions(
+        req.params.companyId
+      )
+      res.render('companies/company', {
+        company: company,
+        companyQuestions: companyQuestions
+      })
     } catch (e) {
       return res.status(404).json(e)
     }
   })
   .put(async (req, res) => {
-    let companyId= req.params.companyId;
-    let requestBody = req.body;
+    let companyId = req.params.companyId
+    let requestBody = req.body
 
     try {
-      companyId = validations.checkId(companyId, 'Company ID');
+      companyId = validations.checkId(companyId, 'Company ID')
       if (!requestBody || Object.keys(requestBody).length === 0) {
-        throw new Error ('You  ust provide data to update a company');
+        throw new Error('You  ust provide data to update a company')
       }
-      requestBody.name = validations.checkString(requestBody.name, "Company's name");
-      requestBody.description = validations.checkString(requestBody.description, "Company's description");
-      requestBody.size = validations.checkString(requestBody.size, 'Size');
-      requestBody.location = validation.checkString(requestBody.location, 'Location');
-    } catch(e) {
-      return res.status(400).json({error: e.message});
+      requestBody.name = validations.checkString(
+        requestBody.name,
+        "Company's name"
+      )
+      requestBody.description = validations.checkString(
+        requestBody.description,
+        "Company's description"
+      )
+      requestBody.size = validations.checkString(requestBody.size, 'Size')
+      requestBody.location = validation.checkString(
+        requestBody.location,
+        'Location'
+      )
+    } catch (e) {
+      return res.status(400).json({ error: e.message })
     }
 
     try {
@@ -105,26 +120,37 @@ router
     }
   })
   .patch(async (req, res) => {
-    let companyId = req.body.companyId;
-    let companyInfo = req.body;
+    let companyId = req.body.companyId
+    let companyInfo = req.body
 
     if (!companyInfo || Object.keys(companyInfo).length === 0) {
-      return res.status(400).json({error: 'You must provdie data to update a company'});
+      return res
+        .status(400)
+        .json({ error: 'You must provdie data to update a company' })
     }
     try {
-      companyId = validations.checkId(companyId, "Company ID");
+      companyId = validations.checkId(companyId, 'Company ID')
 
       if (companyInfo.name) {
-        companyInfo.name = validations.checkString(companyInfo.name, "Company's Name");
+        companyInfo.name = validations.checkString(
+          companyInfo.name,
+          "Company's Name"
+        )
       }
       if (companyInfo.description) {
-        companyInfo.name = validations.checkString(companyInfo.description, 'Description');
+        companyInfo.name = validations.checkString(
+          companyInfo.description,
+          'Description'
+        )
       }
-      if(companyInfo.size) {
-        companyInfo.size = validations.checkString(companyInfo.size, 'Size');
+      if (companyInfo.size) {
+        companyInfo.size = validations.checkString(companyInfo.size, 'Size')
       }
       if (companyInfo.location) {
-        companyInfo.location = validations.checkString(companyInfo.location, 'Location');
+        companyInfo.location = validations.checkString(
+          companyInfo.location,
+          'Location'
+        )
       }
     } catch (e) {
       return res.status(400).json({ error: e.message })
@@ -137,17 +163,17 @@ router
         companyInfo.description,
         companyInfo.size,
         companyInfo.location
-      );
-      res.status(200).json(updatedCompany);
+      )
+      res.status(200).json(updatedCompany)
     } catch (e) {
       return res.status(404).send({ error: e })
     }
   })
   .delete(async (req, res) => {
-    let companyId = req.params.companyId;
+    let companyId = req.params.companyId
 
     try {
-      companyId = validations.checkId(companyId, 'Company ID');
+      companyId = validations.checkId(companyId, 'Company ID')
     } catch (e) {
       return res.status(400).json({ error: e.message })
     }
@@ -158,27 +184,31 @@ router
     } catch (e) {
       return res.status(404).json({ error: e.message })
     }
-  });
+  })
 
-router
- .route('/companiessearch')
- .post(async (req, res) => {
-    try {
-      const {searchByTitle} = req.body;
-      if (!searchByTitle || !searchByTitle.trim()) {
-        res.status(400).render('error', {error: "You must enter a search name"});
-        return;
-      }
-      const companies = await companiesData.getCompanyByTitle(searchByTitle.trim());
-
-      if (companies.length === 0) {
-        res.status(404).render('error', {error: `We're sorry, but no results were found for ${searchByTitle}`});
-      } else {
-        res.render('companies/searchResults', {companies, searchByTitle});
-      }
-    } catch(e) {
-      res.status(500).json({ error: e.message });
+router.route('/companiessearch').post(async (req, res) => {
+  try {
+    const { searchByTitle } = req.body
+    if (!searchByTitle || !searchByTitle.trim()) {
+      res.status(400).render('error', { error: 'You must enter a search name' })
+      return
     }
-  });
+    const companies = await companiesData.getCompanyByTitle(
+      searchByTitle.trim()
+    )
 
-  export default router;
+    if (companies.length === 0) {
+      res
+        .status(404)
+        .render('error', {
+          error: `We're sorry, but no results were found for ${searchByTitle}`
+        })
+    } else {
+      res.render('companies/searchResults', { companies, searchByTitle })
+    }
+  } catch (e) {
+    res.status(500).json({ error: e.message })
+  }
+})
+
+export default router
