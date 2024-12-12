9f6d661430c3b4dbdafa905a23c5d670df2096cc
Michael-Chillemi <michaelchillemi17@gmail.com>
2024-12-11 23:35:00 -0500

    working sign up ajax form

--- public/js/live_mock_interview.js
+++ public/js/live_mock_interview.js
@@ -1,49 +1,50 @@
 
-    // Let's start writing AJAX calls!
-    //Let's get references to our form elements and the div where the todo's will go
-    let mockInterviewForm = document.getElementById('mockInterviewForm'),
-    firstName = document.getElementById('firstName'),
-    LastName = document.getElementById('LastName'),
-    email = document.getElementById('email'),
-    interviewType = document.getElementById('interviewType'),
-    date = document.getElementById('date'),
-    time = document.getElementById('time');
-  
+let mockInterviewForm = document.getElementById("mockInterviewForm");
 
-  
-    //new todo form submission event
-    if (mockInterviewForm) {
-      mockInterviewForm.addEventListener("submit",  (event) => {
-        event.preventDefault();
-        console.log("CLIENT SIDEEE");
-      console.log("INSIDE CLIENT JS");
-      //check for errors 
+if (mockInterviewForm) {
+  mockInterviewForm.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-      if (firstName && 
-        LastName &&
-        email &&
-        interviewType &&
-        date && 
-        time) {
-        //set up AJAX request config
-        let requestConfig = {
-          method: 'POST',
-          url: '/interviewQ',
-          contentType: 'application/json',
-          data: JSON.stringify({
-            firstName: firstName,
-            LastName: LastName,
-            email: email,
-            interviewType: interviewType, 
-            date: date,
-            time: time
-          })
-        };
-        //AJAX Call. Gets the returned HTML data, binds the click event to the link and appends the new todo to the page
-        $.ajax(requestConfig).then(function (responseMessage) {
-          console.log(responseMessage);
-          
-        });
-      }
-    });
-  }
+    let firstName = document.getElementById("firstName").value.trim();
+    let LastName = document.getElementById("LastName").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let interviewType = document.getElementById("interviewType").value.trim();
+    let date = document.getElementById("date").value.trim();
+    let time = document.getElementById("time").value.trim();
+
+    //check for errors
+
+    if (firstName && LastName && email && interviewType && date && time) {
+      //set up AJAX request config
+      let requestConfig = {
+        method: "POST",
+        url: "/createQuestion/interviewQ",
+        contentType: "application/json",
+        data: JSON.stringify({
+          firstName: firstName,
+          LastName: LastName,
+          email: email,
+          interviewType: interviewType,
+          date: date,
+          time: time,
+        }),
+      };
+      //AJAX Call. Gets the returned HTML data, binds the click event to the link and appends the new todo to the page
+      $.ajax(requestConfig).then(function (responseMessage) {
+        // Redirect to the new route
+        if(responseMessage)
+        {
+          window.location.href = '/createQuestion/thankYouSubmission';
+        }
+        else
+        {
+          let errorDiv = document.createElement('div'); 
+          errorDiv.classList.add('error-div'); 
+          errorDiv.textContent = 'ERROR User Update Fail'; 
+          document.getElementById('error-div').appendChild(errorDiv);
+        }
+        
+      });
+    }
+  });
+}
